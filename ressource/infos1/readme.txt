1bf7e17cbfc345a318b94b2d50db4e428b97de29
unknown <mchkourabdallah@gmail.com>
2024-07-21 16:10:55 +0200

    first commit

---
+++ ressource/infos1/readme.txt
@@ -0,0 +1,6 @@
+1er objectif : Développement d’un script pour générer automatiquement un
+plan de traitement du cancer de sein gauche par la technique VMAT sur TPS Raystation
+
+2eme objectif: Développement d’un script permettent d’automatiser l’analyse
+des rapports RTD récupérés par le système AlignRT afin
+d’évaluer la stabilité et reproductibilité des apnées DIBH des patients. 
---
+++ script.js
@@ -0,0 +1,109 @@
+//function im javascript: function myfunction(){}
+//Objet: stock des veleurs On y accède par le point . ou les crochets [] 
+//Les méthodes sont des fonctions définies comme propriétés d'un objet. On les appelle en utilisant le point . suivi du nom de la méthode et des parenthèses
+//Objet: stock des fonctions, commence toujours par new pour creer un nouvel objet a partir d'un class
+//document c'est un objet qui contient des proprietés et des methodes (Ex: document.getElementById())
+//The DOM combines Javascript et HTML together
+//document.body.innerHTML / document.querySelectorAll / document.querySelector('.cls-button').innerHTML
+//method = function saved inside an object
+
+
+// SKILLS : to copy the img and repeat it
+var skillsLogos = document.querySelector('.skills-logos');
+if (skillsLogos) {
+  var copy = skillsLogos.cloneNode(true);
+  var skillsContainer = document.querySelector('.skills-container');
+  if (skillsContainer) {
+    skillsContainer.appendChild(copy);
+  } else {
+    console.error('Element with class "skills-container" not found');
+  }
+} else {
+  console.error('Element with class "skills-logos" not found');
+}
+//
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height){
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
+      })
+    }
+
+  })
+}
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+
+//INFOS HTML
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list')
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = function(){
+  showSlider('next');
+}
+prevDom.onclick = function(){
+  showSlider('prev');
+}
+
+let timeRunning = 3000;
+//let timeAutoNext = 60000;
+let runTimeOut;
+let runAutoRun = setTimeout(() => {      //Pour répéter automatiquement (next et prev)
+  nextDom.click();
+}, timeAutoNext);
+
+function showSlider(type){
+  let itemSlider = document.querySelectorAll('.carousel .list .item');
+  let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+  if (type === 'next'){
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.add('next');
+  }
+  else{
+    let positionLastItem = itemSlider.length - 1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDom.classList.add('next');
+  }
+
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove('next');
+    carouselDom.classList.remove('prev');
+  }, timeRunning);
+
+  clearTimeout(runAutoRun);
+  runAutoRun = setTimeout(() => {   //Pour répéter automatiquement (next et prev)
+    nextDom.click();
+  }, timeAutoNext);
+
+}
+
+
+ 
+
+// initialize aos (library for scroll animation)
+AOS.init()
